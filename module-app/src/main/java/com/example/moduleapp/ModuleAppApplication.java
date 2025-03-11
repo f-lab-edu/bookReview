67abec4e04ec1e849f42b681d20095e6a5c132ad
sumin <sumincathy@naver.com>
2025-03-11 20:43:14 +0900

    로컬 postgreSQL DB 연결

--- module-app/src/main/java/com/example/moduleapp/ModuleAppApplication.java
+++ module-app/src/main/java/com/example/moduleapp/ModuleAppApplication.java
@@ -2,9 +2,11 @@ package com.example.moduleapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.modulecore.domain")  // module-core의 엔티티 패키지 등록
 @ComponentScan(basePackages = {"com.example.moduleapp", "com.example.moduleservice"})
 public class ModuleAppApplication {
 
