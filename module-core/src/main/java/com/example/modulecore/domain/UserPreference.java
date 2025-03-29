ed4e50dd9ccc467122e9cd760b7fa88db5151ff4
sumin <sumincathy@naver.com>
2025-03-29 13:01:06 +0900

    userpreference entity 추가

---
+++ module-core/src/main/java/com/example/modulecore/domain/UserPreference.java
@@ -0,0 +1,24 @@
+package com.example.modulecore.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserPreference {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String genre;
+}
--- module-core/src/main/java/com/example/modulecore/dto/UserPreferencesDto.java
+++ module-core/src/main/java/com/example/modulecore/dto/UserPreferencesDto.java
@@ -12,5 +12,5 @@ import java.util.List;
 @AllArgsConstructor
 public class UserPreferencesDto {
     private Long userId;
-    private List<String> preferredGenres;
+    private List<String> genres;
 }
